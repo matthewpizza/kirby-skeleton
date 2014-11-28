744d61adf14440ff36c5fb4b86754733c49b491a
Matthew Spencer <hello@thisismatthew.com>
2014-11-28 13:27:41 -0500

    Pull in changes from starterkit repo

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,12 +2,21 @@
 <html lang="en">
 <head>
 
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?php echo html($site->title() . ' / ' . $page->title()) ?></title>
+  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+  <meta name="description" content="<?php echo $site->description()->html() ?>">
+  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
-  <?php echo css('assets/css/site.css') ?>
+  <?php echo css('assets/css/main.css') ?>
 
 </head>
-  <body>
+<body>
+
+  <header class="header cf" role="banner">
+    <a class="logo" href="<?php echo url() ?>">
+      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
+    </a>
+    <?php snippet('menu') ?>
+  </header>
